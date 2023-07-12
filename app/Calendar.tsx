d1fe5007b887b76936f0546ecb2c1682d46c8777
Jamie Pond <jamiepond259@gmail.com>
2023-07-11 23:55:23 -0700

    basic look

---
+++ app/Calendar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import GitHubCalendar from "react-github-calendar";
+
+export default function Calendar() {
+  return (
+    <div className="flex flex-col items-center justify-center p-8 space-y-8">
+      <GitHubCalendar username="jamierpond" />
+    </div>
+  )
+}
+
