bcc79d7e04d7e8b27086a6ba38524cd465406806
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-11-22 16:27:01 +0000

    add ads script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,20 @@ export const metadata: Metadata = {
   },
 }
 
+// <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5089904187246568"
+//      crossorigin="anonymous"></script>
+
+function AdsScript() {
+  return (
+    <Script crossOrigin='anonymous'
+      strategy="lazyOnload"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5089904187246568">
+    </Script>
+  );
+}
+
+
+
 export default function RootLayout({
   children,
 }: {
@@ -26,6 +41,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <AdsScript />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
