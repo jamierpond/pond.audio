a7c4f265f04e73961ae12edb90b9270cb547e570
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-11-29 23:03:43 +0000

    fuckyeah

--- app/api/v1/to-gbp/[amount]/[currency]/route.ts
+++ app/api/v1/to-gbp/[amount]/[currency]/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { convertCurrency } from "../../../ConvertCurrency";
+import getSymbolFromCurrency from "currency-symbol-map";
+const countryToCurrency = require("country-to-currency");
 
 export async function GET(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
@@ -7,16 +9,29 @@ export async function GET(request: NextRequest) {
   // "" / api / version / to-gbp / amount / currency
   // 0  / 1   / 2       / 3      / 4      / 5
   const amount = parseFloat(split[4]);
-  const currency = split[5];
+  let currency = split[5];
+
+  const symbol = getSymbolFromCurrency(currency);
+  if (symbol === undefined) { // let's try and guess the user's currency!
+    const country = request.geo?.country || "";
+    currency = countryToCurrency[country];
+    const sym = getSymbolFromCurrency(currency);
+    if (!sym) { // ok we don't know the currency :(
+      // throw Error("we can't figure out the currency, please tell us!");
+      currency = "usd";
+    }
+  }
+
+  console.log(currency);
 
   if (currency.toUpperCase() == "GBP") {
     const usdAmount: number = await convertCurrency(amount, currency, "USD");
-    return NextResponse.json({ "USD": usdAmount});
+    return NextResponse.json({ "USD": usdAmount });
   }
 
   if (currency.toUpperCase() == "USD") {
     const usdAmount: number = await convertCurrency(amount, currency, "GBP");
-    return NextResponse.json({ "GBP": usdAmount});
+    return NextResponse.json({ "GBP": usdAmount });
   }
 
   const usdAmount: number = await convertCurrency(amount, currency, "USD");
