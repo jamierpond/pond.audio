d4d79169d8951cadd022c577ddaba691336bb3e0
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-11-29 21:26:13 +0000

    hello

--- app/pay/[amount]/[currency]/page.tsx
+++ app/pay/[amount]/[currency]/page.tsx
@@ -6,6 +6,30 @@ function getTitle(currency: string, amount: number) {
   return `Pay Jamie ${symbol}${amount}`
 }
 
+function isNumberValid(n: number) {
+  return n !== undefined && !isNaN(n);
+}
+
+function getDescription(currency: string, amount: number, gbpValue: number, usdValue: number) {
+  const isUSD = currency === "USD";
+  const isGBP = currency === "GBP";
+
+  const twoDp = amount.toFixed(2);
+  if (isUSD || isGBP) {
+    return "Please pay Jamie " + getSymbolFromCurrency(currency) + twoDp;
+  }
+
+  if (gbpValue) {
+    return "That's only £" + gbpValue.toFixed(2);
+  }
+
+  if (usdValue) {
+    return "That's only £" + usdValue.toFixed(2);
+  }
+
+  return "Please pay ya boi";
+}
+
 export function generateMetadata({
   params,
   searchParams,
@@ -23,12 +47,7 @@ export function generateMetadata({
 
 function PayPalButton({ currency, amount, gbpValue }: { currency: string, amount: number, gbpValue: number }) {
   const isGbp = currency === "GBP";
-  const gbpValueIsWonky = gbpValue === undefined || isNaN(gbpValue);
-  const showGbpMessage = !isGbp && !gbpValueIsWonky;
-  console.log(gbpValue);
-  console.log("isGbp", isGbp);
-  console.log("showmg", showGbpMessage);
-  console.log(typeof (gbpValue));
+  const showGbpMessage = !isGbp && isNumberValid(gbpValue);
   const gbp2dp = gbpValue.toFixed(2);
   const gbpMessgae = showGbpMessage ? ` (£${gbp2dp})` : "";
   const text = `PayPal` + gbpMessgae;
@@ -41,10 +60,21 @@ function PayPalButton({ currency, amount, gbpValue }: { currency: string, amount
   );
 }
 
-function VenmoButton({ currency, amount }: { currency: string, amount: number }) {
-  return <a
-    className="block mx-auto my-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    href={`https://VENMO.TODO/jamierpond/${currency}/${amount}`}>Venmo</a>;
+function VenmoButton({ currency, amount, givenUsdValue }: { currency: string, amount: number, givenUsdValue?: number }) {
+  const isUSD = currency === "USD";
+  const givenValueIsWonky = givenUsdValue === undefined || isNaN(givenUsdValue);
+  const showUsdMessage = !isUSD && !givenValueIsWonky;
+  const usdMessage = ` ($${givenUsdValue?.toFixed(2)})`
+  const message = showUsdMessage ? usdMessage : "";
+
+  const url = `https://venmo.com/?txn=pay&audience=friends&recipients=jamiepond&amount=${amount}`;
+  return (
+    <a
+      className="block mx-auto my-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      href={url}>
+      Venmo {message}
+    </a>
+  );
 }
 
 export default function Pay({
@@ -52,9 +82,10 @@ export default function Pay({
   searchParams,
 }: {
   params: { currency: string, amount: string },
-  searchParams: { gbp: string }
+  searchParams: { gbp: string, usd: string }
 }) {
   const precomputedGbpValue = parseFloat(searchParams.gbp);
+  const precomputedUsdValue = parseFloat(searchParams.usd);
   const currency = params.currency;
   const amount = parseFloat(params.amount);
   const title = getTitle(currency, amount);
@@ -62,7 +93,7 @@ export default function Pay({
     <>
       <h1>{title}</h1>
       <PayPalButton currency={currency} amount={precomputedGbpValue || amount} gbpValue={precomputedGbpValue} />
-      <VenmoButton currency={currency} amount={amount} />
+      <VenmoButton currency={currency} amount={amount} givenUsdValue={precomputedUsdValue} />
     </>
   );
 }
