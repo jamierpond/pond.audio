1578fb723051d4485f0d25be1bc4598038aa52b6
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-11-29 20:55:31 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <p>I&apos;m Lead Audio Software Engineer at <a href="https://www.mayk.it"><b>mayk</b></a>.</p>
           <p>I studied <b>Sound & Music Computing MSc</b> at <a href="https://www.qmul.ac.uk"><b>Queen Mary University of London</b></a>.</p>
           <p>I spoke at ADC 2021 about <a href="https://www.youtube.com/watch?v=X8dPANPmC7E&ab_channel=JUCE"><b>using compiler intrinsics in your code</b></a>.</p>
-          <p>I spoke at ADC 2023 about <a href="https://conference.audio.dev/session/an-engineers-guide-to-prototyping-building-ai-music-tools-for-the-99/"><b>mayk's approach to prototyping</b></a>.</p>
+          <p>I spoke at ADC 2023 about <a href="https://conference.audio.dev/session/an-engineers-guide-to-prototyping-building-ai-music-tools-for-the-99/"><b>mayk&apos;s approach to prototyping</b></a>.</p>
           <p>I&apos;m a mentor in the ADC 2023 Mentorship Program.</p>
           <p>Please feel free to hit me up!</p>
           <div className="flex flex-row space-x-4">
