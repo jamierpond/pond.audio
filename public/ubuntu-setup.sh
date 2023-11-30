e11e8930c9322c761f6980999acb721b1f0a37d6
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-11-30 12:22:33 -0800

    ub update

--- public/ubuntu-setup.sh
+++ public/ubuntu-setup.sh
@@ -31,3 +31,12 @@ echo "You now need to authorize the github cli with your account"
 
 git clone https://github.com/jamierpond/.config ~/jamie-config
 cp -r ~/jamie-config/* ~/.config
+
+# install packer for nvim
+rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+curl https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+tar xzf nvim-linux64.tar.gz
+sudo mv nvim-linux64/bin/nvim /usr/bin/nvim
