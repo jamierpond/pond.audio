58c1b3c115c9b655fe671885bdacf0011cc23641
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-12-15 20:39:06 -0800

    Make the the images work for /progress !
    This reverts commit 933fcf192c524ba8ec9e02905f7d22a17334cb10.
    
    Revert "a load of bs that didn't work"
    
    This reverts commit ca55371bf4a3ff9d3642058f0535231ce63e9394.
    
    tidy up
    
    and for twitter
    
    try like this again ?
    
    actually update
    
    update for opengraph
    
    fuck...
    
    summary large
    
    400x400
    
    larger...
    
    remove react icons
    
    longer url
    
    smaller images?
    
    sharp downgrade
    
    sharp linux...
    
    fix build
    
    maybe with sharp...?
    
    update all images
    
    actually update the image...
    
    18
    
    thanks chat gpt

--- app/api/v1/progress-image/[progress]/route.ts
+++ app/api/v1/progress-image/[progress]/route.ts
@@ -1,6 +1,53 @@
 import { NextResponse, NextRequest } from "next/server";
-// import { createCanvas } from "canvas";
-const cv = require("canvas");
+import sharp from 'sharp';
+
+async function createImage(amount: number, width: number, height: number) {
+  const lineWithRatio = 0.1;
+  const lineWidth = Math.min(width, height) * lineWithRatio;
+  const radius = Math.min(width, height) / 2 - lineWidth; // 10 is the stroke width
+  const centerX = width / 2;
+  const centerY = height / 2;
+
+  // SVG Circle Element
+  const backgroundCircle = `<circle cx="${centerX}" cy="${centerY}" r="${radius}" stroke="#ddd" stroke-width="${lineWidth}" fill="black"/>`;
+
+  const fullCircle = 2 * Math.PI * radius;
+  const progressLength = (amount / 100) * fullCircle;
+  const offset = fullCircle - progressLength; // Offset the starting point to the top
+
+  const foregroundCircle = `<circle cx="${centerX}" cy="${centerY}" r="${radius}" stroke="green" stroke-width="${lineWidth}" fill="none"
+    stroke-dasharray="${fullCircle}" stroke-dashoffset="${offset}" transform="rotate(-90 ${centerX} ${centerY})"/>`;
+
+  // SVG Check Mark
+  const checkStartX = centerX - radius * 0.4;
+  const checkStartY = centerY + radius * 0.1;
+  const checkMidX = centerX - radius * 0.1;
+  const checkMidY = centerY + radius * 0.4;
+  const checkEndX = centerX + radius * 0.5;
+  const checkEndY = centerY - radius * 0.2;
+
+  const checkMark = `<path d="M${checkStartX},${checkStartY} L${checkMidX},${checkMidY} L${checkEndX},${checkEndY}" stroke="white" stroke-width="${lineWidth * 0.6}" fill="none"/>`;
+
+  // SVG Element, bump for 18
+  const svg = `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+    ${backgroundCircle}
+    ${foregroundCircle}
+    ${checkMark}
+  </svg>`;
+
+    // Convert SVG string to PNG using sharp
+  const pngBuffer = await sharp(Buffer.from(svg))
+    .resize(width, height)
+    .toFormat('png')
+    .toBuffer();
+
+  return new NextResponse(pngBuffer, {
+    headers: {
+      'Content-Type': 'image/png',
+      'Cache-Control': 'public, max-age=31536000, immutable',
+    },
+  });
+}
 
 export async function GET(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
@@ -9,68 +56,13 @@ export async function GET(request: NextRequest) {
   // 0  / 1   / 2       / 3              / 4
   const amount = parseFloat(split[4]);
 
-  const lineWithRatio = 0.1;
-  const width = 512;
-  const height = width;
-  const lineWidth = width * lineWithRatio;
-  const canvas = cv.createCanvas(width, height);
-  const context = canvas.getContext('2d');
+  if (isNaN(amount)) {
+    return new NextResponse("Invalid amount", { status: 400 });
+  }
 
-  // Scale the circles based on the provided width and height
-  const radius = Math.min(width, height) / 2 - lineWidth; // 10 is the stroke width
-  const centerX = width / 2;
-  const centerY = height / 2;
-
-  // fill background black
-  context.fillStyle = 'black';
-  context.fillRect(0, 0, width, height);
-
-  // Background circle
-  context.beginPath();
-  context.arc(centerX, centerY, radius, 0, Math.PI * 2);
-  context.strokeStyle = '#ddd';
-  context.lineWidth = lineWidth;
-  context.stroke();
-
-  // Foreground circle (progress)
-  context.beginPath();
-  context.arc(centerX, centerY, radius, -Math.PI / 2, -Math.PI / 2 + (Math.PI * 2) * (amount / 100));
-  context.strokeStyle = 'green';
-  context.lineWidth = lineWidth;
-  context.stroke();
-
-  // bump, bump
-  // Draw check mark with squared ends and a right angle
-  context.beginPath();
-  context.lineCap = 'butt'; // Squared-off ends
-  context.strokeStyle = 'white';
-  context.lineWidth = lineWidth * 0.6; // Adjust the thickness of the check mark
-
-  // Start point of the check mark
-  const checkStartX = centerX - radius * 0.4;
-  const checkStartY = centerY + radius * 0.1;
-
-  // Middle point of the check mark (right angle)
-  const checkMidX = centerX - radius * 0.1;
-  const checkMidY = centerY + radius * 0.4;
-
-  // End point of the check mark
-  const checkEndX = centerX + radius * 0.5;
-  const checkEndY = centerY - radius * 0.2;
-
-  // Draw the check mark
-  context.moveTo(checkStartX, checkStartY);
-  context.lineTo(checkMidX, checkMidY); // Down to the middle point (right angle)
-  context.lineTo(checkEndX, checkEndY); // Diagonal up to the end point
-  context.stroke();
-
-  const imageData = canvas.createJPEGStream().read();
-  return new NextResponse(imageData, {
-    headers: {
-      "Content-Type": "image/jpeg",
-      "Cache-Control": "public, max-age=31536000, immutable",
-    },
-  });
+  // get width and height from query params
+  const width = parseInt(request.nextUrl.searchParams.get("width") || "400");
+  const height = parseInt(request.nextUrl.searchParams.get("height") || "400");
+  return createImage(amount, width, height);
 }
-
 
--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
-import { FaCheck } from 'react-icons/fa'; // Importing a gamepad icon from react-icons
 import 'tailwindcss/tailwind.css'; // Import TailwindCSS
+import Image from 'next/image';
 import { Metadata } from 'next';
 
 const birthday = new Date('1997-11-12');
 const lifeExpectancy = 105;
 const deathDay = new Date(birthday.getFullYear() + lifeExpectancy, birthday.getMonth(), birthday.getDate());
 const progress = ((Date.now() - birthday.getTime()) / (deathDay.getTime() - birthday.getTime())) * 100;
-const radius = 45;
-const circumference = 2 * Math.PI * radius;
-const strokeDasharray = `${(progress / 100) * circumference} ${circumference}`;
+
+const title = 'Jamie Pond has completed ' + progress.toFixed(2) + '% of life.';
+const twitterImages = [{
+  url: '/api/v1/progress-image/' + progress.toFixed(2),
+  alt: title,
+}];
+
+const openGraphImages = [{
+  url: '/api/v1/progress-image/' + progress.toFixed(2) + '?width=1200&height=630',
+  alt: title,
+}];
+
 
 export async function generateMetadata({
 }: {
   params: {},
   searchParams: {}
 }): Promise<Metadata> {
-  const title = 'Jamie Pond has completed ' + progress.toFixed(2) + '% of life.';
   const desc = 'Born on ' + birthday.toDateString() + ' and expected to die on ' + deathDay.toDateString() + '.';
   return {
     title: title,
     description: desc,
-    twitter: {
+    openGraph: {
       title: title,
       description: desc,
-      images: [{
-        url: '/api/v1/progress-image/' + progress.toFixed(2),
-        alt: 'percent of life completed, ' + progress.toFixed(2) + '%',
-      }],
+      images: openGraphImages,
+    },
+    twitter: {
+      title: title,
+      card: 'summary',
+      description: desc,
+      images: twitterImages,
     },
   };
 }
@@ -47,29 +58,7 @@ export default function Page() {
       >Expected to die on {deathDay.toDateString()}</h2>
       <div className="flex items-center justify-center my-20">
         <div className="relative">
-          <svg height="100" width="100">
-            <circle
-              cx="50"
-              cy="50"
-              r="45"
-              fill="none"
-              stroke="#ddd"
-              strokeWidth="10"
-            />
-            <circle
-              cx="50"
-              cy="50"
-              r="45"
-              fill="none"
-              stroke="green"
-              strokeWidth="10"
-              strokeDasharray={strokeDasharray}
-              transform="rotate(-90 50 50)"
-            />
-          </svg>
-          <div className="absolute inset-0 flex justify-center items-center">
-            <FaCheck size={30} />
-          </div>
+          <Image src={"/api/v1/progress-image/" + progress.toFixed(2)} width="100" height="100" alt="Jamie Pond has completed 100% of life." />
         </div>
       </div>
     </div>
