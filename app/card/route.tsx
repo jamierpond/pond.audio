5a0e67ba9d8d3b94abb51609fe58fa3e5b3f93de
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-12-20 15:53:39 -0800

    move this in here?

--- app/card/route.tsx
+++ app/card/route.tsx
@@ -1,11 +1,12 @@
 import { NextResponse, NextRequest } from "next/server";
 
-const isDev = process.env.NODE_ENV === "development";
-const urlBase = isDev ? "http://localhost:3000" : "https://pond.audio";
-const scanMeUrl = `${urlBase}/add-me`;
-const vcfUrl = `${urlBase}/jamie.vcf`;
 
 export async function GET(req: NextRequest) {
+  const isDev = process.env.NODE_ENV === "development";
+  const urlBase = isDev ? "http://localhost:3000" : "https://pond.audio";
+  const scanMeUrl = `${urlBase}/add-me`;
+  const vcfUrl = `${urlBase}/jamie.vcf`;
+
   const userAgent = req.headers.get("user-agent")?.toLowerCase();
   const isMobile = userAgent?.includes("android") || userAgent?.includes("iphone");
   if (isMobile) {
