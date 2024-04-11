c24375d699694b85753d03ef894f611eab449c98
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-04-11 23:31:16 +0100

    lol

--- app/card/route.ts
+++ app/card/route.ts
@@ -1,15 +1,4 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const isDev = process.env.NODE_ENV === "development";
-  const urlBase = isDev ? "http://localhost:3000" : "https://pond.audio";
-  const scanMeUrl = `${urlBase}/add-me`;
-  const vcfUrl = `${urlBase}/jamie.vcf`;
-
-  const userAgent = req.headers.get("user-agent")?.toLowerCase();
-  const isMobile = userAgent?.includes("android") || userAgent?.includes("iphone");
-  if (isMobile) {
-    return NextResponse.redirect(vcfUrl);
-  }
-  return NextResponse.redirect(scanMeUrl);
+import { NextResponse } from "next/server";
+export async function GET() {
+  return NextResponse.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
