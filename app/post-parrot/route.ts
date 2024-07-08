21c1482da32aff374c12cbd29d18e38c7ad545de
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-07-08 14:20:14 +0100

    add logging

--- app/post-parrot/route.ts
+++ app/post-parrot/route.ts
@@ -2,6 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
   // parrot back the json given
   const json = await request.json();
+  console.log("Parrotting back", json);
   return new NextResponse(JSON.stringify(json), {
     headers: {
       "content-type": "application/json",
