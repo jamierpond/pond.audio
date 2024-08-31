25858202384e6dbcf14ad62793c02d8127257810
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-08-30 18:43:10 -0700

    temp bm phone thing

---
+++ app/card/page.tsx
@@ -0,0 +1,17 @@
+
+export default function Page() {
+  return ( <>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl">My Phone is lost!</h1>
+      <div
+      className="w-3/4 p-4 "
+      >
+      <p
+      >My name is Jamie Pond and I'm camping at Camp Illuminaughty at 9:00 & E</p>
+      <hr />
+      <p>If you could please return my phone to to me, I would be so grateful!</p>
+    </div>
+    </div>
+  </>
+  );
+}
