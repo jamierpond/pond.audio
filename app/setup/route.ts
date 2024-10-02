398710d7e4118d47f5f504a06fa3923f49f6d272
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-10-02 13:16:40 -0700

    also let bootstrap work

--- app/setup/route.ts
+++ app/setup/route.ts
@@ -1,7 +1,4 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export async function GET(_: NextRequest) {
+import { NextResponse } from "next/server";
+export async function GET() {
   return NextResponse.redirect("https://github.com/jamierpond/bootstrap/raw/main/ubuntu-setup.sh");
 }
-
-
