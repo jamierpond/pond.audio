a1458c2ec0527250f388d658ff152b8e508cd608
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-10-11 11:34:18 -0500

    refactored

--- app/neer/page.tsx
+++ app/neer/page.tsx
@@ -3,16 +3,21 @@ import { data } from "./data";
 import { useEffect, useRef } from "react";
 const pako = require('pako');
 
-// data is a string
-export default function Page() {
+// decodes from base64 and then unzips the data
+function decode_gzip_base64(data: string): string {
   const decodedBase64 = atob(data);
   const array = new Uint8Array(decodedBase64.length);
   for (let i = 0; i < decodedBase64.length; i++) {
     array[i] = decodedBase64.charCodeAt(i);
   }
-  const unzippedData = pako.inflate(array);
+  return pako.inflate(array);
+}
 
+// data is a string
+export default function Page() {
+  const unzippedData = decode_gzip_base64(data);
   const audioRef = useRef<HTMLAudioElement>(null);
+
   useEffect(() => {
     if (audioRef.current) {
       const blob = new Blob([unzippedData], { type: "audio/mpeg" });
