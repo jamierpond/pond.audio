428212ae4da3c31ffcd7a3a7a691b0ede04bc432
Jamie Pond <jamiepond259@gmail.com>
2024-11-17 21:39:23 -0800

    typing

--- app/lemon/route.ts
+++ app/lemon/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
-  return NextResponse.json({ message: "Hello, Emily! You're the internet!" });
+  return NextResponse.json({ message: "Hello, Emily! You're on the internet!" });
 }
