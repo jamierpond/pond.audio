ee162a873dee3362e34bc668408287d498ac403e
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2025-02-12 21:20:52 -0800

    emily

---
+++ app/emily/route.ts
@@ -0,0 +1,7 @@
+
+// https://youtube.com/clip/UgkxOiyCJIRgl4RWWFcBf10zrClhX1sFLWub?si=P6ed-G_0A2xdlpBG
+
+import { NextResponse } from 'next/server';
+export function GET() {
+  return NextResponse.redirect('https://youtube.com/clip/UgkxOiyCJIRgl4RWWFcBf10zrClhX1sFLWub?si=P6ed-G_0A2xdlpBG');
+}
