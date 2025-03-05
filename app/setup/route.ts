de65762fbed7666aab45e64ff540bf337f1e5e52
Jamie Pond <jamiepond259@gmail.com>
2025-03-05 14:59:50 -0800

    update bootstrap

--- app/setup/route.ts
+++ app/setup/route.ts
@@ -1,4 +1,4 @@
 import { NextResponse } from "next/server";
 export async function GET() {
-  return NextResponse.redirect("https://github.com/jamierpond/bootstrap/raw/main/ubuntu-setup.sh");
+  return NextResponse.redirect("https://github.com/jamierpond/.config/raw/main/ubuntu-setup.sh");
 }
