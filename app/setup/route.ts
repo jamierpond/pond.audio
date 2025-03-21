a7151cf1bffe9376f612fdd69020060950e9147d
Jamie Pond <jamiepond259@gmail.com>
2025-03-20 21:04:55 -0700

    wip

--- app/setup/route.ts
+++ app/setup/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server";
 export async function GET() {
+  // this file is like totally different
+  console.log("Serving ubuntu-setup.sh");
   return NextResponse.redirect("https://github.com/jamierpond/.config/raw/main/ubuntu-setup.sh");
 }
