492b0c365e3cc295b8ff1042dc89c8561f07d70a
Jamie Pond <jamiepond259@gmail.com>
2025-10-14 20:03:48 -0700

    rename

--- app/yapi/schema/route.ts
+++ app/yapi/schema/route.ts
@@ -1,4 +1,4 @@
 import { NextResponse } from "next/server";
 export async function GET() {
-  return NextResponse.redirect("https://raw.githubusercontent.com/jamierpond/yapit/refs/heads/main/yapit.schema.json");
+  return NextResponse.redirect("https://raw.githubusercontent.com/jamierpond/yapi/refs/heads/main/yapi.schema.json");
 }
